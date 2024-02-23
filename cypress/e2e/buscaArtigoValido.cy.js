0c6346930ce4a02072fe30f0a026cc911751ebd3
Vidiviso <vdiniz.med@gmail.com>
2024-02-23 12:44:29 -0300

    Roda BlogAgiVictor 12:44 23/02

---
+++ cypress/e2e/buscaArtigoValido.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+import loc from '../support/locators/locators'
+
+
+describe('Busca determinado artigo válido', () => {
+    //Redirecionamento para tela home do blog
+    beforeEach(() => {
+        cy.visit("https://blogdoagi.com.br/")
+    })
+
+    it('Pesquisa um artigo pelo ícone de lupa da tela principal', () => {
+        //Garante que as imagens, menus e principais informações são corretamente carregados
+        cy.get(loc.CARROSSEL_DE_SLIDES).should('be.visible')
+        cy.get(loc.IMG_ULTIMASDOBLOG).should('be.visible')
+        cy.get(loc.IMG_TDSOBREEMPRESTIMO).should('be.visible')
+        cy.get(loc.CAMPO_EMAIL).should('be.visible')
+        cy.get(loc.RODAPE).should('exist')
+        //Realiza o clique no botão de lupa e a busca do artigo por palavras
+        cy.get(loc.ICONE_DE_LUPA).click()
+        cy.get(loc.CAMPO_DE_BUSCA).type('Como fazer PIX?'+'{downarrow}{enter}', { delay: 150 })
+        //Verifica a existência de uma nova tela com os resultados da busca efetuada
+        cy.get(loc.TEXTO_RESULTADO_BUSCA).should('exist')
+        //Valida os dados exibidos e relacionados a busca efetuada
+        cy.get(loc.ARTIGO1).should('be.visible')
+        cy.get(loc.SUB_TITULO3).should('contain', 'Qual a diferença entre Pix, TED e DOC?')
+        cy.get(loc.PAG1).should('contain', '1')
+        cy.get(loc.PROXIMO).should('exist')
+    })
+
+})
---
+++ cypress/support/locators/locators.js
@@ -0,0 +1,17 @@
+const locators = {
+        CARROSSEL_DE_SLIDES: '.wp-block-uagb-post-carousel',
+        IMG_ULTIMASDOBLOG: '.uagb-post__image-position-top > :nth-child(2)',
+        IMG_TDSOBREEMPRESTIMO: '.uagb-block-be168cfe > :nth-child(2) > .uagb-post__image',
+        CAMPO_EMAIL: '.uagb-forms-email-input',
+        RODAPE: '.site-footer-primary-section-2',
+        ICONE_DE_LUPA: '.site-header-above-section-right > .ast-builder-layout-element',
+        CAMPO_DE_BUSCA: '#search-field',
+        TEXTO_RESULTADO_BUSCA: '.page-title',
+        ARTIGO1: '#post-3105 > .ast-post-format-',
+        SUB_TITULO3: '#post-2444 > .ast-post-format- > .post-content > .entry-title > a',
+        PAG1: '.current',
+        PROXIMO: '.next',
+        MSG_DE_RETORNO: '.page-content > p',
+        CAMPO_DE_PESQUISA: '.widget > .search-form > label > #search-field'
+}
+export default locators;
