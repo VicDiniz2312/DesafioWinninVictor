5739498e79b34e97a6ee38aa0f3d22db31742ea3
Vidiviso <vdiniz.med@gmail.com>
2024-07-09 21:39:53 -0300

    DesafioWinninVictor

--- cypress/e2e/Busca.cy.js
+++ cypress/e2e/Busca.cy.js
@@ -18,7 +18,7 @@ describe('Acesso a página da Wikipedia', () => {
         cy.get(loc.INPUT_BUSCA).should('be.visible').type('Brasil'+'{downarrow}{enter}', { delay: 250 })
         //Garante a existência dos principais elementos nessa Segunda página 
         cy.get(loc.LOGO_TOPO_WIKIPEDIA2).should('exist')
-        cy.get(loc.TXT_BRASIL).should('contain', 'Brasil')
+        cy.get(loc.TXT_BRASIL).should('exist')
         cy.get(loc.PAINEL).should('be.visible')
         //Realiza um scroll para descer a página
         cy.scrollTo('bottom', { duration: 1000 })
